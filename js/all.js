b77810efdf4fddd69f1bdb07f67c98404dc32dd8
hzlzh <hzlzh.dev@gmail.com>
2012-05-21 14:04:36 +0800

    beta 1.4

--- js/all.js
+++ js/all.js
@@ -3,12 +3,15 @@ jQuery(function() {
   jQuery('a[href*=#]').click(function() {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
       var $target = jQuery(this.hash);
-      $target = $target.length && $target || jQuery('[name=' + this.hash.slice(1) + ']');
+      var $url = this.hash.slice(1);
+      var $scrollTime = 500;
+      $target = $target.length && $target || jQuery('[name=' + $url + ']');
       if ($target.length) {
         var targetOffset = $target.offset().top;
         jQuery('html,body').animate({
           scrollTop: targetOffset
-        }, 500);
+        }, $scrollTime);
+        setTimeout("window.location.hash = encodeURIComponent('"+url+"');",$scrollTime+100)
         return false;
       }
     }
